9252c6814d5d2d848641ac4a325eca4e73de37ad
anandshankarbala <anandshankarbala@gmail.com>
2020-10-08 22:57:21 +0530

    Product model added and started with dummy data

--- product-api-annotation/src/main/java/com/wiredbraincoffee/productapiannotation/ProductApiAnnotationApplication.java
+++ product-api-annotation/src/main/java/com/wiredbraincoffee/productapiannotation/ProductApiAnnotationApplication.java
@@ -1,13 +1,32 @@
 package com.wiredbraincoffee.productapiannotation;
 
+import com.wiredbraincoffee.productapiannotation.model.Product;
+import com.wiredbraincoffee.productapiannotation.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class ProductApiAnnotationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApiAnnotationApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(ProductRepository repository){
+		return args -> {
+			Flux<Product> productFlux = Flux.just(
+					new Product(null,"Chai",2.99),
+					new Product(null,"Poha",7),
+					new Product(null,"Dosa",10)
+					).flatMap(repository::save);
+
+			productFlux.thenMany(repository.findAll())
+					.subscribe(System.out::println);
+		};
 	}
 
 }
---
+++ product-api-annotation/src/main/java/com/wiredbraincoffee/productapiannotation/model/Product.java
@@ -0,0 +1,22 @@
+package com.wiredbraincoffee.productapiannotation.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Product {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private double price;
+
+}
